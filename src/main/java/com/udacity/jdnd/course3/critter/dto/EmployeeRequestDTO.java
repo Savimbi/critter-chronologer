b3aa06a7d931bc45650609e091939f2b6dd7142e
savimbi <dudosa212@gmail.com>
2021-01-24 20:34:23 +0900

    debbug id related error

--- src/main/java/com/udacity/jdnd/course3/critter/dto/EmployeeRequestDTO.java
+++ src/main/java/com/udacity/jdnd/course3/critter/dto/EmployeeRequestDTO.java
@@ -1,6 +1,7 @@
 package com.udacity.jdnd.course3.critter.dto;
 
 import com.udacity.jdnd.course3.critter.entity.EmployeeSkill;
+import lombok.Data;
 
 import java.time.LocalDate;
 import java.util.Set;
@@ -9,23 +10,10 @@ import java.util.Set;
  * Represents a request to find available employees by skills. Does not map
  * to the database directly.
  */
+@Data
 public class EmployeeRequestDTO {
     private Set<EmployeeSkill> skills;
     private LocalDate date;
 
-    public Set<EmployeeSkill> getSkills() {
-        return skills;
-    }
 
-    public void setSkills(Set<EmployeeSkill> skills) {
-        this.skills = skills;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
 }
